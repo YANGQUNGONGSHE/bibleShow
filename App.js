8b7fc7bf411383cb7ca865d08b1145d72c8694a3
汪亮亮 <wll1048812995@163.com>
2018-11-20 18:51:24 +0800

    ttt
    tttt

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View,Button,TextInput,StatusBar,ViewPagerAndroid} from 'react-native';
+import {StyleSheet, Text, View,Button,TextInput,StatusBar,ViewPagerAndroid,TouchableOpacity} from 'react-native';
 import { red } from 'ansi-colors';
 // import {connect} from 'react-redux';
 // import {getAddItem,getRemoveItem,getChangeInputValue} from './js/store/actionCreator';
@@ -16,90 +16,43 @@ class App extends Component{
 
 
   render() {
-    // let {inputValue,list,inputChangeValue,addItemClick,deleteItemClick} = this.props;
     return (
-      <ViewPagerAndroid 
-        pageMargin = {20}
-        peekEnabled = {true}
-        style={styles.container}
-        initialPage = {0}
-        >
-        {/* <View style ={styles.inPutContainer}>
-          <TextInput 
-            value = {inputValue}
-            onChangeText = {inputChangeValue}
-          />
+      <View style = {styles.container}>
+        <View style = {styles.but}>
           <Button
-            onPress = {addItemClick}
-          >提交</Button>
-        </View>
-          {
-            list.map((item,index,arr)=> 
-          <Text 
-            key = {item}
+            color ={'red'}
+            title = {'测试点击按钮'}
             onPress = {()=>{
-              // deleteItemClick(index)
+                // alert('我被点击了');
             }}
-            >{item}</Text>)
-          } */}
-          {/* <StatusBar
-            barStyle = {'default'}
-          /> */}
-          <View style = {styles.bar} key = {1}>
-            <Text>第一</Text>
-          </View>
-          <View style = {styles.bar} key = {2}>
-            <Text>第二</Text>
-          </View>
-          <View style = {styles.bar} key = {3}>
-            <Text>第三</Text>
-          </View>
-          
-      </ViewPagerAndroid>
+          />
+        </View>
+        <TouchableOpacity 
+          style = {styles.numtext}
+          onPress = {()=>{
+            alert('dier');
+          }}
+          >
+          <Text>NUMTWO</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
-// const mapStateToProps = (state) =>{
-
-//   return {
-//     list:state.list,
-//     inputValue:state.inputValue,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) =>{
-//   return {
-
-//     inputChangeValue(value){
-//       const action = getChangeInputValue(value);
-//       dispatch(action);
-//     },
-//     addItemClick(){
-//       const action =  getAddItem();
-//       dispatch(action);
-//     },
-//     deleteItemClick(index){
-//       const action = getRemoveItem(index);
-//       dispatch(action);
-//     }
-//   };
-// };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor:'red',
   },
-  inPutContainer:{
-    flexDirection:'column',
-    marginBottom:20,
-  },
-  bar:{
-    backgroundColor:'red',
+  but:{
     margin:20,
+    width:100,
+  },
+  numtext:{
+    width:100,
+    height:35,
+    margin:20,
+    backgroundColor:'#0000ff'
   }
 });
-
-// export default connect (mapStateToProps,mapDispatchToProps)(App);
 export default App;
